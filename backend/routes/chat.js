62c338d3d9ce1bf731e6891839beb5b08d9ad407
Nishan Singh <nishansingh2480@gmail.com>
2024-12-27 19:48:28 +0530

    env-setup

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const chats = require('../data/data');
-router.get('/',(req,res)=>{
+console.log(chats)
+;router.get('/',(req,res)=>{
     res.send(chats);
 })
-
+router.get('/:id',(req,res)=>{
+    // console.log(req.params.id);
+    const singlechat = chats.find((c)=>c._id===req.params.id);
+    res.send(singlechat);
+})
 
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const dotenv = require("dotenv");
 const cors = require('cors');
+dotenv.config();
 const bodyParser = require('body-parser');
 
 const registerRoutes = require('./routes/register');
@@ -16,7 +18,7 @@ app.use('/register', registerRoutes);
 app.use('/send-otp', otpRoutes);
 app.use('/api/chat',chatroute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
