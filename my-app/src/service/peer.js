3fd799f8be840d6044a3cf8c61fb8d6bea4641dd
Nishan Singh <nishansingh2480@gmail.com>
2025-01-16 20:03:12 +0530

    L

--- my-app/src/service/peer.js
+++ my-app/src/service/peer.js
@@ -5,6 +5,9 @@ class PeerService {
           iceServers: [
             {
               urls: [
+                "stun:stun1.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302",
                 "stun:stun.l.google.com:19302",
                 "stun:global.stun.twilio.com:3478",
               ],
